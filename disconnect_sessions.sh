e3121b68f5c28cb666f53c8926074b8deb0d6ad6
Adam Forester <aforester@silverdogstudios.com>
2019-02-18 13:10:03 -0600

    v1

--- disconnect_sessions.sh
+++ disconnect_sessions.sh
@@ -1,8 +1,5 @@
-printf "\nWhat is the IP address or Name of the Domain or SMS you want to check?\n"
-read DOMAIN
-
 printf "\nCreating Session Cleanup\n"
-mgmt_cli -r true -d $DOMAIN show sessions limit 500 --format json | jq -r ' .objects[] | "mgmt_cli -s id.txt discard uid " + .uid ' >kill_sessions.txt
+mgmt_cli -r true show sessions limit 500 --format json | jq -r ' .objects[] | "mgmt_cli -s id.txt discard uid " + .uid ' >kill_sessions.txt
 
 sed -i '1s/^/mgmt_cli -r true login > id.txt\n/' kill_sessions.txt
 echo "mgmt_cli -s id.txt publish" >> kill_sessions.txt
